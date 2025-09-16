6e1256c81f55bbcf3ed56bf9bae2dbead3c0001c
Mksm1929 <maksimvarnin31@gmail.com>
2025-09-16 22:35:18 +0400

    Update src/pages/orders/orders.tsx
    Co-authored-by: fortyone-41 <75414851+fortyone-41@users.noreply.github.com>

--- src/pages/orders/orders.tsx
+++ src/pages/orders/orders.tsx
@@ -67,7 +67,7 @@ const Orders = () => {
                     СВЕТЛАНЫ ЮРЬЕВОЙ</i></b> – <b>НА ПРОСПЕКТЕ АЛЕКСАНДРОВСКОЙ ФЕРМЫ. Д. 2</b></p>
             <p>ЗАПИСЬ ПО ТЕЛЕФОНУ <a href="tel:+79811890747">+79811890747</a> (WHATSAPP, TELEGRAM)</p>
             <div className="orders-feedback-btn">
-                <Button onClick={textOutput} title="Обратная связь"></Button>
+                <Button onClick={textOutput} title="Обратная связь" />
             </div>
         </div>
         <Modal isOpened={false} onClose={() => { }} title="" card={{
